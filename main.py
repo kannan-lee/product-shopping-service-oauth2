82f1a3763c6b45c8e7164f9e512011573cadea99
lee <kannanlee345@gmail.com>
2022-03-06 21:53:07 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import speech_recognition as sr
+import numpy as np
 import pyttsx3
 from playsound import playsound
+import sounddevice as sd
+import speech_recognition as sr
+from scipy.io.wavfile import write
 #import pocketsphinx
 import tkinter as tk
 
 listener = sr.Recognizer()
 
-if True:
-#try:
+
+try:
     with sr.Microphone() as source:
-        print("speak something")
-        fs = 44100
+        listener.adjust_for_ambient_noise(source, duration=2)
+        print("Set minimum energy threshold to {}".format(listener.energy_threshold))
+        print("Say something")
         voice = listener.listen(source)
-        write('output.wav', fs, voice)
-        playsound(voice)
+        with open("output.wav", "wb") as file:
+            file.write(voice.get_wav_data())
+        playsound("output.wav")
         command= listener.recognize_google(voice,language = 'en-IN', show_all = True)
-        #command = [c.lower() for c in command]
+        command = [c.lower() for c in command]
         print(command)
-#except Exception as e:
-#    print(e)
+except Exception as e:
+    print(e)
